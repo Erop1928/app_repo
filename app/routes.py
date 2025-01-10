f528dbbd5f1c7a035c6acb111d45f0722744fe0b
Egor <Erop@1928.engineering>
2025-01-10 13:17:49 +0300

    Refactor APK upload process to use Config for upload folder
    - Updated the upload_version route to reference the upload folder from Config instead of current_app.config, improving code clarity and maintainability.
    - Ensured that the directory for uploads is created if it does not exist, enhancing robustness in the file upload process.
    
    These changes contribute to a more organized and reliable APK upload workflow.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request, send_from_directory, jsonify
+from flask import Blueprint, render_template, redirect, url_for, flash, request, send_from_directory, jsonify, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import User, Category, Application, ApkVersion, VersionFlag, Role, UserActionLog, OneTimeDownloadLink
 from app.forms import (LoginForm, CategoryForm, ApplicationForm, UploadApkForm, 
@@ -330,7 +330,7 @@ def upload_version(id):
                 return redirect(url_for('main.upload_version', id=application.id))
 
             # Создаем директорию для загрузок, если её нет
-            upload_folder = current_app.config['UPLOAD_FOLDER']
+            upload_folder = Config.UPLOAD_FOLDER
             os.makedirs(upload_folder, exist_ok=True)
             
             # Генерируем уникальное имя файла
