d3f2d77f20dbd1113ec99e339443e38ccf08d1ff
Egor <Erop@1928.engineering>
2025-01-10 11:14:42 +0300

    fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Проверка и создание необходимых директорий
+mkdir -p /app/instance
+mkdir -p /app/uploads
+
+# Установка правильных прав доступа
+chmod 777 /app/instance
+chmod 777 /app/uploads
+
 # Инициализация базы данных, если она еще не существует
 if [ ! -f "/app/instance/app.db" ]; then
     echo "Initializing database..."
